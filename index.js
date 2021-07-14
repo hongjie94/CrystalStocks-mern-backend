b0ed7cf628af6b11dc61c1db55c0f3f0d19a2061
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-07-14 03:30:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ import User from './models/User.js';
 // -------------------- App Config --------------------
 const app = express();
 dotenv.config();
-// const pusher = new Pusher({
-//   appId: process.env.PUSHER_ID,
-//   key: process.env.PUSHER_KEY,
-//   secret: process.env.PUSHER_SECRET,
-//   cluster: process.env.PUSHER_CLUSTER,
-//   useTLS: true
-// });
+
 
 // -------------------- Middleware --------------------
 app.use(express.json());
@@ -31,17 +25,17 @@ app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.set('trust proxy', 1); 
 
-app.use(session({
-cookie:{
-    secure: true,
-    maxAge:60000
-       },
-store: new RedisStore(),
-secret: "anything",
-saveUninitialized: true,
-resave: false
-}));
-
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+    }
+  }))
 
 // Initialize passport
 app.use(passport.initialize());
@@ -113,38 +107,12 @@ passport.deserializeUser((id, done) => {
 mongoose.connect(process.env.MONGO_URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: true 
+    useUnifiedTopology: true,
+    useFindAndModify: true 
 });
-
-// const transCollection = mongoose.connection.collection("transactions");
 
 mongoose.connection.once('open', () => {
 	console.log("Connected to MongoDB!");
-	//Runing pusher
-	// const changeStream = transCollection.watch();
-
-	// changeStream.on("change", (change) => {
-	// 	console.log("data change", change);
-	// 	if (change.operationType === "insert") {
-
-	// 		console.log("Pusher insert!");
-			
-	// 		pusher.trigger("transactions", "inserted", {
-	// 			_id: change.fullDocument._id,
-	// 			user_id: change.fullDocument.user_id,
-	// 			stocks: change.fullDocument.stocks
-	// 		});
-	//  	}else if( change.operationType === "update") {
-	// 		console.log("Pusher update!");
-
-	// 		pusher.trigger("transactions", "updated", {
-	// 			stocks: change.updateDescription.updatedFields.stocks
-	// 		});
-	// 	}else {
-	// 		console.log("Pusher Error!");
-	// 	}
-	// });
 }).on('error', (error) => {
 	console.log(`Connection error: ${error}`);
 });
