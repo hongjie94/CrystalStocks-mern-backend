b8b49ac74f84767ca8be866219b9636e47f8c7c2
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-07-16 00:48:02 -0400

    Update AuthRoutes.js

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -13,7 +13,7 @@ router.get('/google',
 router.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/', session: true }),
   (req, res) =>{
-    res.redirect('https://nifty-goldberg-c7ab73.netlify.app/holdings');
+    res.redirect('https://nifty-goldberg-c7ab73.netlify.app');
 });
 
 // Register user 
