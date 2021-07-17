7c42ff91439cd4eab2fbe169b40898caabf0ec09
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-07-17 16:53:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ dotenv.config();
 // -------------------- Middleware --------------------
 app.use(express.json());
 
-app.use(cors({ origin: 'https://crystalstocks.netlify.app', credentials: true }));
+app.use(cors({ origin: ['https://crystalstocks.netlify.app', 'https://crystalstocks-react.web.app'], credentials: true }));
 
 app.set('trust proxy', 1); 
 
 app.use(
 	session({
-		secret: "anything",
+		secret: "secretcode",
 		resave: true,
 		saveUninitialized: true,
     cookie: {
       sameSite: "none",
       secure: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+      maxAge: 1000 * 60 * 60 * 24 // One Day
     }
 }));
 
