e96ce1b4495f1221d4b14cdc9c4a491efa9695d4
Zack <Home@MacBook-Air.local>
2021-07-22 03:28:51 -0400

    set session

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(
 		resave: true,
 		saveUninitialized: true	,
     cookie: {
-			sameSite: 'strict',
+			sameSite: 'none',
 			secure: true,
     	maxAge: 1000 * 60 * 60 * 24  // One Day 
 		} 
