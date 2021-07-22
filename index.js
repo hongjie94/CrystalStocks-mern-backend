ba7d844b1de78d1045c3c5b9fefaccf82c424b3f
Zack <Home@MacBook-Air.local>
2021-07-22 18:14:46 -0400

    add new origin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin:'https://crystalstocks.netlify.app', 
+		origin:['https://crystalstocks.netlify.app', 'https://crystalstocks-react-ac962.web.app'], 
 		credentials: true 
 	})
 );
