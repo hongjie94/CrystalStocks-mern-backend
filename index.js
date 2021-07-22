78b0b363ccf9276d15970a91875579bb1469ae5b
Zack <Home@MacBook-Air.local>
2021-07-22 02:27:18 -0400

    orgin expression

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
 	cors({ 
-		origin:'https://crystalstocks.netlify.app', 
+		origin: /netlify\.app$/, 
 		// origin:'http://localhost:3000', 
 		credentials: true 
 	})
