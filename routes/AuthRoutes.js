32d1b1c29838e76949e0d770ec65cc43d4fe5f18
Zack <Home@MacBook-Air.local>
2021-07-22 19:27:33 -0400

    update AuthRoutes.js

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -84,7 +84,7 @@ router.get("/getuser", (req, res) => {
     return res.send(sendData);
   }
   if (!req.user) {
-    return res.send('get user data fail');
+    return res.send('get user data fail!');
   }
 });
 
